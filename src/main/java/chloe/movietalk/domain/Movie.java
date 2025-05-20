231d40d6c2d91387fe540d610a7ca70894e60d43
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-05-20 22:37:47 +0900

    feat: create Movie entity and MovieRepository interface

---
+++ src/main/java/chloe/movietalk/domain/Movie.java
@@ -0,0 +1,41 @@
+package chloe.movietalk.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@ToString
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "code_fims", length = 50, nullable = false)
+    private String codeFIMS;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String synopsis;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Column(name = "prod_year")
+    private Integer prodYear;
+
+    @Builder
+    public Movie(String codeFIMS, String title, String synopsis, LocalDate releaseDate, Integer prodYear) {
+        this.codeFIMS = codeFIMS;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.releaseDate = releaseDate;
+        this.prodYear = prodYear;
+    }
+}
---
+++ src/main/java/chloe/movietalk/repository/MovieRepository.java
@@ -0,0 +1,12 @@
+package chloe.movietalk.repository;
+
+import chloe.movietalk.domain.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByTitleContaining(String keyword);
+}
