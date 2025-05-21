bd6fe1270d6714d89c63732f3731a233b031f3dc
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-05-21 13:24:31 +0900

    test: add unit tests for create and duplicate check logic in MovieService

---
+++ src/test/java/chloe/movietalk/service/MovieServiceTest.java
@@ -0,0 +1,71 @@
+package chloe.movietalk.service;
+
+import chloe.movietalk.domain.Movie;
+import chloe.movietalk.dto.request.MovieRequestDto;
+import chloe.movietalk.dto.response.MovieDto;
+import chloe.movietalk.repository.MovieRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class MovieServiceTest {
+
+    MovieService movieService;
+
+    @Mock
+    MovieRepository movieRepository;
+
+    @BeforeEach
+    public void beforeEach() {
+        movieService = new MovieServiceImpl(movieRepository);
+    }
+
+    @Test
+    @DisplayName("영화 생성 성공")
+    public void createMovieSuccess() {
+        // given
+        MovieRequestDto requestDto = MovieRequestDto.builder()
+                .title("테스트용 영화")
+                .codeFIMS("123123")
+                .build();
+
+        given(movieRepository.save(any(Movie.class)))
+                .willReturn(requestDto.toEntity());
+
+        // when
+        MovieDto movie = movieService.createMovie(requestDto);
+
+        // then
+        assertThat(movie.getTitle()).isEqualTo(requestDto.getTitle());
+        assertThat(movie.getCodeFIMS()).isEqualTo(requestDto.getCodeFIMS());
+    }
+
+    @Test
+    @DisplayName("영화 생성 실패 - code 중복")
+    public void createMovieException() {
+        // given
+        MovieRequestDto requestDto = MovieRequestDto.builder()
+                .title("테스트용 영화")
+                .codeFIMS("123123")
+                .build();
+
+        given(movieRepository.findByCodeFIMS("123123"))
+                .willReturn(Optional.of(requestDto.toEntity()));
+
+        // when then
+        IllegalStateException e = Assertions.assertThrows(IllegalStateException.class,
+                () -> movieService.createMovie(requestDto));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 영화입니다.");
+    }
+}
