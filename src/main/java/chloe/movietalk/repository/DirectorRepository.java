8a9d5a6bfb6028d2add0a8e6ee8bf544618027ce
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-05-21 22:35:24 +0900

    feat: add method to find Director by name containing in repository

--- src/main/java/chloe/movietalk/repository/DirectorRepository.java
+++ src/main/java/chloe/movietalk/repository/DirectorRepository.java
@@ -4,6 +4,9 @@ import chloe.movietalk.domain.Director;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DirectorRepository extends JpaRepository<Director, Long> {
+    List<Director> findByNameContaining(String keyword);
 }
