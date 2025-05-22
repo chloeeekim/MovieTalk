f2f854419a43457e46347a50fbee68ff9af11980
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-05-22 21:55:43 +0900

    feat: create JPA repository for Actor and MovieActor entity

--- src/main/java/chloe/movietalk/repository/ActorRepository.java
+++ src/main/java/chloe/movietalk/repository/ActorRepository.java
@@ -2,9 +2,11 @@ package chloe.movietalk.repository;
 
 import chloe.movietalk.domain.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ActorRepository extends JpaRepository<Actor, Long> {
 
     List<Actor> findByNameContaining(String keyword);
