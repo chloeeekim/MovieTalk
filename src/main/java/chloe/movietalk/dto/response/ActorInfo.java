0c01a20651f76e360fbd3f930cbb046c94791027
chloe jungah kim <43295963+chloeeekim@users.noreply.github.com>
2025-05-22 17:56:21 +0900

    feat: create MovieDetailResponse DTO including ActorInfo list

---
+++ src/main/java/chloe/movietalk/dto/response/ActorInfo.java
@@ -0,0 +1,34 @@
+package chloe.movietalk.dto.response;
+
+import chloe.movietalk.domain.Actor;
+import chloe.movietalk.domain.Gender;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ActorInfo {
+
+    private Long id;
+    private String name;
+    private Gender gender;
+    private String country;
+
+    @Builder
+    public ActorInfo(Long id, String name, Gender gender, String country) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.country = country;
+    }
+
+    public static ActorInfo fromEntity(Actor actor) {
+        return ActorInfo.builder()
+                .id(actor.getId())
+                .name(actor.getName())
+                .gender(actor.getGender())
+                .country(actor.getCountry())
+                .build();
+    }
+}
---
+++ src/main/java/chloe/movietalk/dto/response/MovieDetailResponse.java
@@ -0,0 +1,55 @@
+package chloe.movietalk.dto.response;
+
+import chloe.movietalk.domain.Movie;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+public class MovieDetailResponse {
+
+    private Long id;
+    private String codeFIMS;
+    private String title;
+    private String synopsis;
+    private LocalDate releaseDate;
+    private Integer prodYear;
+    private DirectorInfo director;
+    private List<ActorInfo> actors;
+
+    @Builder
+    public MovieDetailResponse(Long id,
+                               String codeFIMS,
+                               String title,
+                               String synopsis,
+                               LocalDate releaseDate,
+                               Integer prodYear,
+                               DirectorInfo director,
+                               List<ActorInfo> actors) {
+        this.id = id;
+        this.codeFIMS = codeFIMS;
+        this.title = title;
+        this.synopsis = synopsis;
+        this.releaseDate = releaseDate;
+        this.prodYear = prodYear;
+        this.director = director;
+        this.actors = actors;
+    }
+
+    public static MovieDetailResponse fromEntity(Movie movie) {
+        return MovieDetailResponse.builder()
+                .id(movie.getId())
+                .codeFIMS(movie.getCodeFIMS())
+                .title(movie.getTitle())
+                .synopsis(movie.getSynopsis())
+                .releaseDate(movie.getReleaseDate())
+                .prodYear(movie.getProdYear())
+                .director(movie.getDirector() == null ? null : DirectorInfo.fromEntity(movie.getDirector()))
+                .actors(movie.getActors() == null ? null : movie.getActors().stream().map(ActorInfo::fromEntity).toList())
+                .build();
+    }
+}
